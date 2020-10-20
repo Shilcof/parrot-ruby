366c361ab6ff6404fed4151ef8723f512df163cd
Shilcof <robert.shilcof@gmail.com>
2020-10-20 20:33:31 +0100

    commit tree fcc22c19ffa11ab563d496d32ab6929b2d939c0a

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(input = "")
-    puts phrase = input != "" ? "Pretty bird!" : "Squawk!"
+def parrot(phrase = "Squawk!")
+    puts phrase 
     phrase
 end
